a3495bb6b31d5b2bd3ff30c44d409a7ecec89441
Lars Hanisch <dvb@flensrocker.de>
2014-11-20 20:50:18 +0100

    implement default behaviour in cPluginSkinDisplayMenu

--- pluginskin.h
+++ pluginskin.h
@@ -13,9 +13,9 @@ namespace libvdrskinservice {
     cPluginSkinDisplayMenu(void) {};
     virtual ~cPluginSkinDisplayMenu(void) {};
 
-    virtual void SetPluginMenu(const char *PluginName, int MenuId, int Type, bool init) = 0;
+    virtual void SetPluginMenu(const char *PluginName, int MenuId, int Type, bool init) {};
     virtual bool SetItemValues(const cKeyValueList<cString> *StringValues, const cKeyValueList<int> *IntValues, const cKeyValueList< cList< cKeyValueList<cString> > > *LoopValues, int Index, bool Current, bool Selectable) { return false; };
-    virtual bool SetTextValues(const cKeyValueList<cString> *StringValues, const cKeyValueList<int> *IntValues, const cKeyValueList< cList< cKeyValueList<cString> > > *LoopValues) = 0;
+    virtual bool SetTextValues(const cKeyValueList<cString> *StringValues, const cKeyValueList<int> *IntValues, const cKeyValueList< cList< cKeyValueList<cString> > > *LoopValues) { return false; };
   };
 
 
