e584a34bc5f0ae02cbdffb279038696af2e064dd
Lars Hanisch <dvb@flensrocker.de>
2014-11-20 02:44:49 +0100

    started interface independend of skindesigner

---
+++ pluginskin.c
@@ -0,0 +1,148 @@
+#include "pluginskin.h"
+
+
+// --- cTokenStore -----------------------------------------------------------
+
+libvdrskinservice::cTokenStore::cTokenStore(void)
+ : stringValues(NULL)
+ , intValues(NULL)
+ , loopValues(NULL)
+{
+}
+
+libvdrskinservice::cTokenStore::~cTokenStore(void)
+{
+  ClearTokens();
+}
+
+void libvdrskinservice::cTokenStore::ClearTokens(void)
+{
+  delete stringValues;
+  stringValues = NULL;
+  delete intValues;
+  intValues = NULL;
+  delete loopValues;
+  loopValues = NULL;
+}
+
+void libvdrskinservice::cTokenStore::AddStringToken(const char *Key, cString *Value)
+{
+  if (stringValues == NULL)
+     stringValues = new cKeyValueList<cString>();
+
+  stringValues->Add(new cKeyValuePair<cString>(Key, Value));
+}
+
+void libvdrskinservice::cTokenStore::AddIntToken(const char *Key, int Value)
+{
+  if (intValues == NULL)
+     intValues = new cKeyValueList<int>();
+
+  intValues->Add(new cKeyValuePair<int>(Key, new int(Value)));
+}
+
+void libvdrskinservice::cTokenStore::AddLoopToken(const char *LoopName, cKeyValueList<cString> *Tokens)
+{
+  if (loopValues == NULL)
+     loopValues = new cKeyValueList< cList< cKeyValueList<cString> > >();
+
+  cKeyValuePair< cList< cKeyValueList<cString> > > *loop = loopValues->Find(LoopName);
+  if (loop == NULL) {
+     loop = new cKeyValuePair< cList< cKeyValueList<cString> > >(LoopName, new cList< cKeyValueList<cString> >());
+     loopValues->Add(loop);
+     }
+  loop->Value().Add(Tokens);
+}
+
+// --- cPluginSkin -----------------------------------------------------------
+
+cVector<libvdrskinservice::cPluginSkin*> libvdrskinservice::cPluginSkin::pluginSkins;
+
+libvdrskinservice::cPluginSkin::cPluginSkin(const char *Name, cTheme *Theme)
+ : cSkin(Name, Theme)
+ , currentDisplayMenu(NULL)
+{
+  pluginSkins.Append(this);
+}
+
+libvdrskinservice::cPluginSkin::~cPluginSkin(void)
+{
+  for (int i = 0; i < pluginSkins.Size(); i++) {
+      if (pluginSkins.At(i) == this) {
+         pluginSkins.Remove(i);
+         break;
+         }
+      }
+}
+
+cSkinDisplayMenu *libvdrskinservice::cPluginSkin::DisplayMenu(void)
+{
+  cPluginSkinDisplayMenu *displayMenu = OnDisplayMenu();
+  currentDisplayMenu = displayMenu;
+  return displayMenu;
+}
+
+libvdrskinservice::cPluginSkinDisplayMenu *libvdrskinservice::cPluginSkin::CurrentDisplayMenu(void)
+{
+  cSkin *current = Skins.Current();
+  for (int i = 0; i < pluginSkins.Size(); i++) {
+      cPluginSkin *skin = pluginSkins.At(i);
+      if (skin == current)
+         return skin->currentDisplayMenu;
+      }
+  return NULL;
+}
+
+// --- cPluginSkinOsdMenu ----------------------------------------------------
+
+libvdrskinservice::cPluginSkinOsdMenu::cPluginSkinOsdMenu(const char *PluginName, const char *Title, int c0, int c1, int c2, int c3, int c4)
+ : cOsdMenu(Title, c0, c1, c2, c3, c4)
+ , init(true)
+ , pluginName(PluginName)
+ , type(mtList)
+ , text("")
+{
+}
+
+void libvdrskinservice::cPluginSkinOsdMenu::SetPluginMenu(int MenuId, eMenuType Type)
+{
+  type = Type;
+  cPluginSkinDisplayMenu *displayMenu = cPluginSkin::CurrentDisplayMenu();
+  if (displayMenu)
+     displayMenu->SetPluginMenu(*pluginName, MenuId, Type, init);
+  init = false;
+}
+
+void libvdrskinservice::cPluginSkinOsdMenu::Display(void)
+{
+  cPluginSkinDisplayMenu *displayMenu = cPluginSkin::CurrentDisplayMenu();
+  if (type == mtText) {
+     if (displayMenu && displayMenu->SetMenuValues(stringValues, intValues, loopValues))
+        displayMenu->Flush();
+     else {
+        DisplayMenu()->Clear();
+        DisplayMenu()->SetText(*text, false);
+        DisplayMenu()->Flush();
+        }
+     return;
+     }
+  cOsdMenu::Display();
+}
+
+// --- cPluginSkinOsdItem ----------------------------------------------------
+
+libvdrskinservice::cPluginSkinOsdItem::cPluginSkinOsdItem(cPluginSkinDisplayMenu *Menu, eOSState State)
+ : cOsdItem(State)
+ , menu(Menu)
+{
+}
+
+libvdrskinservice::cPluginSkinOsdItem::~cPluginSkinOsdItem(void)
+{
+}
+
+void libvdrskinservice::cPluginSkinOsdItem::SetMenuItem(cSkinDisplayMenu *DisplayMenu, int Index, bool Current, bool Selectable)
+{
+  if ((menu == NULL) || !menu->SetItemPlugin(stringValues, intValues, loopValues, Index, Current, Selectable))
+     DisplayMenu->SetItem(Text(), Index, Current, Selectable);
+}
---
+++ pluginskin.h
@@ -0,0 +1,131 @@
+#ifndef __LIBVDRSKINSERVICE_PLUGINSKIN_H
+#define __LIBVDRSKINSERVICE_PLUGINSKIN_H
+
+#include <vdr/osdbase.h>
+#include <vdr/skins.h>
+
+namespace libvdrskinservice {
+
+  template<class T> class cKeyValuePair : public cListObject {
+    private:
+      cString  key;
+      T  *value;
+
+    public:
+      cKeyValuePair(const char *Key, T *Value) {
+        key = Key;
+        value = Value;
+        }
+
+      virtual ~cKeyValuePair(void) { delete value; }
+
+      virtual int Compare(const cListObject &ListObject) const
+      {
+        cKeyValuePair* kv = (cKeyValuePair*)&ListObject;
+        return strcmp(*key, *(kv->key));
+      }
+
+      const char *Key(void) { return *key; }
+      T &Value(void) const { return *value; }
+  };
+
+
+  template<class T> class cKeyValueList : public cListObject, public cList< cKeyValuePair<T> > {
+    public:
+      cKeyValuePair<T> *Find(const char *Key) {
+        for (cKeyValuePair<T> *kv = this->cList< cKeyValuePair<T> >::First(); kv; kv = this->cList< cKeyValuePair<T> >::Next(kv)) {
+            if (strcmp(Key, kv->Key()) == 0)
+               return kv;
+            }
+        return NULL;
+        }
+  };
+
+
+  class cTokenStore {
+  protected:
+    cKeyValueList<cString> *stringValues;
+    cKeyValueList<int> *intValues;
+    cKeyValueList< cList< cKeyValueList<cString> > > *loopValues;
+
+  public:
+    cTokenStore(void);
+    virtual ~cTokenStore(void);
+
+    void ClearTokens(void);
+    void AddStringToken(const char *Key, cString *Value); // Value must be instantiated with new
+    void AddIntToken(const char *Key, int Value);
+    void AddLoopToken(const char *LoopName, cKeyValueList<cString> *Tokens); // Value must be instantiated with new
+  };
+
+
+  class cPluginSkinDisplayMenu : public cSkinDisplayMenu {
+  public:
+    virtual void SetPluginMenu(const char *PluginName, int MenuId, int Type, bool init) = 0;
+    virtual bool SetItemPlugin(const cKeyValueList<cString> *StringValues, const cKeyValueList<int> *IntValues, const cKeyValueList< cList< cKeyValueList<cString> > > *LoopValues, int Index, bool Current, bool Selectable) { return false; };
+    virtual bool SetMenuValues(const cKeyValueList<cString> *StringValues, const cKeyValueList<int> *IntValues, const cKeyValueList< cList< cKeyValueList<cString> > > *LoopValues) = 0;
+  };
+
+
+  class cPluginSkin : public cSkin {
+  private:
+    static cVector<cPluginSkin*> pluginSkins;
+    cPluginSkinDisplayMenu *currentDisplayMenu;
+
+  protected:
+    // override this instead of DisplayMenu
+    virtual cPluginSkinDisplayMenu *OnDisplayMenu(void) = 0;
+
+  public:
+    cPluginSkin(const char *Name, cTheme *Theme = NULL);
+    virtual ~cPluginSkin(void);
+
+    virtual cSkinDisplayMenu *DisplayMenu(void);
+
+    static cPluginSkinDisplayMenu *CurrentDisplayMenu(void);
+  };
+
+
+  enum eMenuType {
+      mtList,
+      mtText
+  };
+
+
+  class cPluginSkinOsdMenu : public cOsdMenu, protected cTokenStore {
+  private:
+    bool init;
+    cString pluginName;
+    eMenuType type;
+    cString text;
+
+  protected:
+    bool Init(void) const { return init; }
+    const char *PluginName(void) const { return *pluginName; }
+    eMenuType MenuType(void) const { return type; }
+
+    void SetPluginMenu(int MenuId, eMenuType Type);
+    void SetText(const char *Text) { text = Text; }
+
+  public:
+    cPluginSkinOsdMenu(const char *PluginName, const char *Title, int c0 = 0, int c1 = 0, int c2 = 0, int c3 = 0, int c4 = 0);
+    virtual ~cPluginSkinOsdMenu(void) {};
+
+    virtual void Display(void);
+  };
+
+
+  class cPluginSkinOsdItem : public cOsdItem, public cTokenStore {
+  private:
+    cPluginSkinDisplayMenu *menu;
+
+  public:
+    cPluginSkinOsdItem(cPluginSkinDisplayMenu *Menu, eOSState State = osUnknown);
+    virtual ~cPluginSkinOsdItem(void);
+
+    virtual void SetMenuItem(cSkinDisplayMenu *DisplayMenu, int Index, bool Current, bool Selectable);
+  };
+
+}
+
+#endif // __LIBVDRSKINSERVICE_PLUGINSKIN_H
